452aa6727a2b52495a014b3879de690f9c5ac570
Subodh Gaisamudre <subodh.gaisamudre@gmail.com>
2025-08-21 14:15:49 +0530

    Update api.py

--- Hangman_Model/api.py
+++ Hangman_Model/api.py
@@ -1,1 +1,13 @@
+import json
+from hangman import HangmanAI
 
+ai = HangmanAI()
+
+def api_call(input_json):
+    data = json.loads(input_json)
+    next_guess = ai.suggest_letter(
+        current_state=data["currentWordState"],
+        guessed_letters=data["guessedLetters"],
+        guesses_remaining=data["guessesRemaining"]
+    )
+    return json.dumps({"nextGuess": next_guess})
