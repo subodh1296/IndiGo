c24e482fb942fea8711a11e1af75cfcca2e4957b
Subodh Gaisamudre <subodh.gaisamudre@gmail.com>
2025-08-21 14:15:21 +0530

    Update play_test.py

--- Hangman_Model/play_test.py
+++ Hangman_Model/play_test.py
@@ -1,1 +1,10 @@
+import json
+from api import api_call
 
+# Example simulation
+test_input = {
+    "currentWordState": "_ _ e _ a n",
+    "guessedLetters": ["e", "a", "n"],
+    "guessesRemaining": 4
+}
+print(api_call(json.dumps(test_input)))
